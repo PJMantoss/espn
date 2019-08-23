7fe38b544a2f9cd6a2d673a939b678be1f515c32
PJ Mantoss <joelptoss@gmail>
2019-08-23 20:54:06 +0100

    Create Comments Models and View files

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+
+    def create
+        @message = Message.find(params[:message_id])
+        @message = @message.comments.create(comment_params)
+        @comment.user_id = current_user.user_id
+
+        if @comment.save
+            redirect_to message_path(@message)
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+
+end
