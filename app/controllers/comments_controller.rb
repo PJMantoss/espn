1eabc1f79a6d01b36fbe5c669b973fd934ec088a
PJ Mantoss <joelptoss@gmail>
2019-08-23 21:42:55 +0100

    Add Edit and Delete for Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
+    before_action :find_message, only: [:create, :edit, :update, :destroy]
+    before_action :find_comment, only: [:edit, :update, :destroy]
 
     def create
-        @message = Message.find(params[:message_id])
-        @message = @message.comments.create(comment_params)
+        @comment = @message.comments.create(comment_params)
         @comment.user_id = current_user.id
 
         if @comment.save
@@ -15,10 +16,31 @@ class CommentsController < ApplicationController
     def edit
     end
 
+    def update
+        if @comment.update(comment_params)
+            redirect_to message_path(@message)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @comment.destroy
+        redirect_to message_path(@message)
+    end
+
     private
 
     def comment_params
         params.require(:comment).permit(:content)
+    end
+
+    def find_message
+        @message = Message.find(params[:message_id])
+    end
+
+    def find_comment
+        @comment = @message.comments.find(params[:id])
     end
 
 end
